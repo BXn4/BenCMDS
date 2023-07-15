5d2983decccf3fd6900b8055c41b5cb6e544079f
Bence <78733248+BXn4@users.noreply.github.com>
2023-07-15 11:36:01 +0200

    Update Wikipedia.java

--- src/main/java/bxn4/bencmds/commands/Wikipedia.java
+++ src/main/java/bxn4/bencmds/commands/Wikipedia.java
@@ -1,5 +1,6 @@
 package bxn4.bencmds.commands;
 
+import Jwiki.Jwiki;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -18,32 +19,64 @@ public class Wikipedia extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         String command = event.getName();
-        EmbedBuilder eb = new EmbedBuilder();
         switch (command) {
             case "wiki":
-                String query = event.getOption("query").getAsString();
+                String query = null;
                 try {
-                    URL wikiUrl = new URL("https://wikipedia.org/w/api.php?action=opensearch&search=" + query);
-                    HttpURLConnection connection = (HttpURLConnection) wikiUrl.openConnection();
-                    connection.setRequestMethod("GET");
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                    }
-                    reader.close();
-                    JSONArray data = new JSONArray(response.toString());
-                    JSONArray titles = data.getJSONArray(1);
-                    JSONArray urls = data.getJSONArray(3);
-                    for (int i = 0; i < titles.length(); i++) {
+                    query = event.getOption("query").getAsString();
+                }
+                catch (NullPointerException e) {
+                }
+                if(query != null && !query.isEmpty()) {
+                    EmbedBuilder eb = new EmbedBuilder();
+                    try {
+                        URL wikiUrl = new URL("https://wikipedia.org/w/api.php?action=opensearch&search=" + query);
+                        HttpURLConnection connection = (HttpURLConnection) wikiUrl.openConnection();
+                        connection.setRequestMethod("GET");
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        reader.close();
+                        JSONArray data = new JSONArray(response.toString());
+                        JSONArray titles = data.getJSONArray(1);
+                        JSONArray urls = data.getJSONArray(3);
                         eb.setTitle("Query: " + query);
-                        eb.addField(titles.getString(i), "🔗 [" + titles.getString(i) + "]" + "(" +  urls.getString(i) + ")", false);
                         eb.setColor(new Color(98, 177, 246));
+                        if(titles.length() > 0) {
+                            for (int i = 0; i < titles.length(); i++) {
+                                eb.addField(titles.getString(i), "🔗 [" + titles.getString(i) + "]" + "(" + urls.getString(i) + ")", false);
+                            }
+                            eb.setFooter("Articles found: " + titles.length());
+                        }
+                        else {
+                            eb.setFooter("Articles found: 0");
+                        }
+                        event.replyEmbeds(eb.build()).setEphemeral(true).queue();
+                    } catch (MalformedURLException e) {
+                    } catch (IOException e) {
                     }
-                    event.replyEmbeds(eb.build()).setEphemeral(true).queue();
-                } catch (MalformedURLException e) {
-                } catch (IOException e) {
+                }
+                else {
+                    EmbedBuilder eb = new EmbedBuilder();
+                    try {
+                        String article = event.getOption("article").getAsString();
+                        Jwiki jwiki = new Jwiki(article);
+                        String titleRaw = jwiki.getDisplayTitle();
+                        String title = titleRaw.replace("<span class=\"mw-page-title-main\">", "").replace("</span>", "");
+                        String url = title.replace(" ", "_");
+                        eb.addField(title, jwiki.getExtractText(), false);
+                        eb.addField("🔗 Source: ", "[Wikipedia]" + "(https://wikipedia.org/wiki/" + url + ")", false);
+                        eb.setThumbnail(jwiki.getImageURL());
+                        event.replyEmbeds(eb.build()).setEphemeral(true).queue();
+                    }
+                    catch (NullPointerException e) {
+                        eb.setTitle("Not found");
+                        eb.setColor(Color.RED);
+                        event.replyEmbeds(eb.build()).setEphemeral(true).queue();
+                    }
                 }
                 break;
         }
