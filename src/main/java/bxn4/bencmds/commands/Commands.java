6a202ccdb1c9faa148f58d52126917160d162d32
Bence <78733248+BXn4@users.noreply.github.com>
2023-07-17 00:12:03 +0200

    Update Commands.java

--- src/main/java/bxn4/bencmds/commands/Commands.java
+++ src/main/java/bxn4/bencmds/commands/Commands.java
@@ -24,8 +24,9 @@ public class Commands extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         List<CommandData> commandDataList = new ArrayList<>();
-        commandDataList.add(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("ping", "Test the bot response time."));
-        commandDataList.add(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("wiki", "Search on Wikipedia").addOption(OptionType.STRING, "query", "The search query to get articles with the same name").addOption(OptionType.STRING, "article", "Get the article"));
+        commandDataList.add(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("ping", "Test the bot response time"));
+        commandDataList.add(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("wiki", "Search on Wikipedia").addOption(OptionType.STRING, "query", "Search query to get articles with the same name").addOption(OptionType.STRING, "article", "Get the article"));
+        commandDataList.add(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("weather", "Get the current weather information").addOption(OptionType.STRING, "place", "City or country", true).addOption(OptionType.STRING, "temperature_unit", "Set the temperature unit", false, true).addOption(OptionType.STRING, "speed_unit", "Set the speed unit", false, true).addOption(OptionType.STRING, "precipitation_unit", "Set the precipitation unit", false, true));
         event.getJDA().updateCommands().addCommands(commandDataList).queue();
     }
 }
